826bf50c6c5776922b4018ca16d1e8058aefdef3
Simon Vareille <si.vareille@gmail.com>
2022-09-23 17:17:19 +0200

    Add main activity to manually set time

---
+++ app/src/main/java/com/github/simonvareille/onlykey_android_settime/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.simonvareille.onlykey_android_settime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
