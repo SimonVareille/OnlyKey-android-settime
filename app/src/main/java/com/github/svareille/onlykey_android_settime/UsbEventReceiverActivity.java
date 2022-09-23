a9ceebc0583f891fcd03153b80c0754c65928673
Simon Vareille <si.vareille@gmail.com>
2022-09-23 15:19:19 +0200

    Add source files

---
+++ app/src/main/java/com/github/svareille/onlykey_android_settime/UsbEventReceiverActivity.java
@@ -0,0 +1,42 @@
+package com.github.svareille.onlykey_android_settime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.os.Parcelable;
+
+public class UsbEventReceiverActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        Intent intent = getIntent();
+        if (intent != null)
+        {
+            if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED))
+            {
+                Parcelable usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                // Create a new intent and put the usb device in as an extra
+                Intent deviceAttachedIntent = new Intent(getApplicationContext(), SetTime.class);
+                //deviceAttachedIntent.setAction(ACTION_USB_DEVICE_ATTACHED);
+                deviceAttachedIntent.putExtra(UsbManager.EXTRA_DEVICE, usbDevice);
+                //deviceAttachedIntent.setClass(this, SetTime.class);
+
+                startService(deviceAttachedIntent);
+            }
+        }
+
+        // Close the activity
+        finish();
+    }
+}
